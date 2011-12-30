2ba4783add98469730dd93a501f5c90c2ff776a1
David Li <taweili@yahoo.com>
2011-12-30 14:09:09 +0800

    add file to raw

--- src/come/featha/kungfudoll/Welcome.java
+++ src/come/featha/kungfudoll/Welcome.java
@@ -2,14 +2,19 @@ package come.featha.kungfudoll;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class Welcome extends Activity
 {
+	private VideoView vView;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+         vView = (VideoView)findViewById(R.id.videoView1);
     }
 }
