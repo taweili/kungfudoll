7a355e47f1acc9e207be7b4850255e891ac6a962
HE Qichen <heqichen@yahoo.com.cn>
2012-02-06 03:35:24 +0800

    demo

--- src/come/featha/kungfudoll/Welcome.java
+++ src/come/featha/kungfudoll/Welcome.java
@@ -2,50 +2,63 @@ package come.featha.kungfudoll;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 
 import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.widget.MediaController;
-import android.widget.TextView;
-import android.widget.VideoView;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebSettings.PluginState;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
+import java.util.Scanner;
 
 public class Welcome extends Activity {
-	private VideoView vView;
-	private TextView sView; // score view
-	private TextView tView; // time view
+
 	private ServerSocket serverSocket;
-    private Handler handler = new Handler();
+
+    
+    private WebView wView;
+    
+    private static String score = null;
+    private static String time = null;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		//full-screen
+    	
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);  
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
 		setContentView(R.layout.main);
-		sView = (TextView) findViewById(R.id.textView2);
-		tView = (TextView) findViewById(R.id.TextView01);
 
 		Thread st = new Thread(new ServerThread());
 		st.start();
+		
+		
+		wView = (WebView)this.findViewById(R.id.webView1);
+		
+		wView.getSettings().setJavaScriptEnabled(true);
+		wView.getSettings().setPluginsEnabled(true);
+		wView.getSettings().setPluginState(PluginState.ON);
+		
+    	
+        
+		//wView.loadUrl("http://192.168.11.2:8090/release.html");
+		wView.loadUrl("http://127.0.0.1:8090/release.html");
+		//wView.loadUrl("http://192.168.11.3/flash/release.html");
 
-		vView = (VideoView) findViewById(R.id.videoView1);
-		MediaController controller = new MediaController(this);
-		vView.setMediaController(controller);
-		String filePath="android.resource://" + getPackageName() + "/" + R.raw.hit;
-
-		vView.setVideoURI(Uri.parse(filePath));
-		//vView.setVideoURI(Uri.parse("http://blog.ardublock.com/wp-content/uploads/2012/01/hit3.mp4"));
-		vView.requestFocus();
-		vView.start();
 	}
 	
 	@Override
@@ -78,36 +91,41 @@ public class Welcome extends Activity {
 	}
 
 	public class ServerThread implements Runnable {
-		private String line = null;
 
 		public void run() {
-			try {
+			try 
+			{
 				serverSocket = new ServerSocket(8080);
-				handler.post(new Runnable() {
-					public void run() {
-						sView.setText("9");
-					}
-				});
-				while (true) {
-					Socket client = serverSocket.accept();
+				
+				
+				while (true)
+				{
+					try
+					{
+						if (serverSocket.isClosed())
+						{
+							break;
+						}
+						Socket client = serverSocket.accept();
+						Log.e("socket", "a client in: " + client.getRemoteSocketAddress() + " :" + client.getPort());
+						Scanner sin = new Scanner(client.getInputStream());
+						String line = sin.nextLine();
+						
+						score = line;
+						wView.loadUrl("javascript:updateScore(\"" + score + "\");");
+						Log.e("socket", "update it");
 
-					try {
-						BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                        while ((line = in.readLine()) != null) {
-        					handler.post(new Runnable() {
-        						public void run() {
-        							String str[] = line.split(" ");
-        							sView.setText(str[0]);
-        							tView.setText(str[1]);
-        						}
-        					});                        	
-                        }
-					} catch (Exception e) {
-						Log.e("socket", e.getMessage());
+						sin.close();
+						client.close();
 					}
-					client.close();
+					catch (Exception e)
+					{
+						Log.e("socket", "sth: " + e.getMessage());
+					}
 				}
-			} catch (Exception e) {
+			}
+			catch (Exception e) 
+			{
 				Log.e("socket", "exception: " + e.getMessage());
 			}
 		}
